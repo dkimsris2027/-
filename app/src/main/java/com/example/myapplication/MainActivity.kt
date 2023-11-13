72cd5cfcac3356725d87d44a5ef4d9786b188be7
daehyunkim <kgus2033@gmail.com>
2023-11-13 16:49:54 +0900

    dsjfklsjf

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn);
+        btn.setOnClickListener {
+            btn.text = "sdjkfsjkfdskjfsdj";
+        }
     }
 }
